0246c7575f6a23a7d12faf4ff8142ef6db6b88cb
halil <halilbadem47@gmail.com>
2023-02-09 18:11:04 +0300

    Fix styles

--- src/Home/index.js
+++ src/Home/index.js
@@ -23,93 +23,96 @@ const Home = () => {
           <a href="#">TR</a>/<a href="#">EN</a>
         </nav>
       </header>
-      <section className="content">
+
+      <section className="content mt-4">
         <img className="w-100" src={banner} alt="" />
       </section>
-      <section className="cards mt-4">
-        <div className="cards__item">
-          <div className="cards__header -active">
-            <h3 className="cards__title">1.hedefimize ulaştık!</h3>
+
+      <main className="container">     
+        <section className="cards mt-4">
+          <div className="cards__item">
+            <div className="cards__header -active">
+              <h3 className="cards__title">1.hedefimize ulaştık!</h3>
+            </div>
+            <div className="cards__footer">
+              <b className="cards__count">22 Adet Konteyner</b>
+              <p>1 Milyon TL</p>
+            </div>
           </div>
-          <div className="cards__footer">
-            <b className="cards__count">22 Adet Konteyner</b>
-            <p>1 Milyon TL</p>
+          <div className="cards__item">
+            <div className="cards__header">
+              <h3 className="cards__title">1.hedefimize ulaştık!</h3>
+            </div>
+            <div className="cards__footer">
+              <b className="cards__count">22 Adet Konteyner</b>
+              <p>1 Milyon TL</p>
+            </div>
           </div>
+        </section>
+
+        <section className="donation mt-4">
+          <img className="donation__img" src={ahbapLogo} alt="" />
+          <div className="donation__inner">
+            <p>Depremzedelerimize Yardım için</p>
+            <button type="button" className="btn btn-lg btn-danger">
+              BAĞIŞ YAP
+            </button>
+          </div>
+          <img className="donation__img" src={tatkoLogo} alt="" />
+        </section>
+
+        <section className="multibanner mt-4">
+          <img className="multibanner__image" src={photo1} alt="" />
+          <img className="multibanner__image" src={photo2} alt="" />
+          <img className="multibanner__image" src={photo3} alt="" />
+        </section>
+
+        <section className="transfer mt-4">
+          <p>
+            Ahbap derneğinin resmi hesaplarına, dünyanın her yerinden döviz
+            havalesi yapılabilmektedir.
+          </p>
+          <p>
+            Yurdışından yapılacak ödemeler için xxxx swift kodunu
+            kullanabilirsiniz.
+          </p>
+          <p>
+            Yardımlarınızı iletebileceğiniz Ahbap Derneği'ne ait hesap numaraları:
+          </p>
+        </section>
+
+        <div className="accounts mt-4">
+          <section className="account">
+            <h5 className="account__item">TL HESABI</h5>
+            <div className="account__item">1021-1380059</div>
+            <div className="account__item">IBAN</div>
+            <div className="account__item">TR12000640000011380059</div>
+          </section>
+
+          <section className="account">
+            <h5 className="account__item">TL HESABI</h5>
+            <div className="account__item">1021-1380059</div>
+            <div className="account__item">IBAN</div>
+            <div className="account__item">TR12000640000011380059</div>
+          </section>
+
+          <section className="account">
+            <h5 className="account__item">TL HESABI</h5>
+            <div className="account__item">1021-1380059</div>
+            <div className="account__item">IBAN</div>
+            <div className="account__item">TR12000640000011380059</div>
+          </section>
+
+          <section className="account">
+            <h5 className="account__item">TL HESABI</h5>
+            <div className="account__item">1021-1380059</div>
+            <div className="account__item">IBAN</div>
+            <div className="account__item">TR12000640000011380059</div>
+          </section>
         </div>
-        <div className="cards__item">
-          <div className="cards__header">
-            <h3 className="cards__title">1.hedefimize ulaştık!</h3>
-          </div>
-          <div className="cards__footer">
-            <b className="cards__count">22 Adet Konteyner</b>
-            <p>1 Milyon TL</p>
-          </div>
-        </div>
-      </section>
-
-      <section className="donation mt-4">
-        <img src={ahbapLogo} alt="" />
-        <div className="donation__inner">
-          <p>Depremzedelerimize Yardım için</p>
-          <button type="button" className="btn btn-lg btn-danger">
-            BAĞIŞ YAP
-          </button>
-        </div>
-        <img src={tatkoLogo} alt="" />
-      </section>
-
-      <section className="multibanner mt-4">
-        <img className="multibanner__image" src={photo1} alt="" />
-        <img className="multibanner__image" src={photo2} alt="" />
-        <img className="multibanner__image" src={photo3} alt="" />
-      </section>
-
-      <section className="transfer">
-        <p>
-          Ahbap derneğinin resmi hesaplarına, dünyanın her yerinden döviz
-          havalesi yapılabilmektedir.
-        </p>
-        <p>
-          Yurdışından yapılacak ödemeler için xxxx swift kodunu
-          kullanabilirsiniz.
-        </p>
-        <p>
-          Yardımlarınızı iletebileceğiniz Ahbap Derneği'ne ait hesap numaraları:
-        </p>
-      </section>
-
-      <div className="accounts">
-        <section className="account">
-          <h5 className="account__item">TL HESABI</h5>
-          <div className="account__item">1021-1380059</div>
-          <div className="account__item">IBAN</div>
-          <div className="account__item">TR12000640000011380059</div>
-        </section>
-
-        <section className="account">
-          <h5 className="account__item">TL HESABI</h5>
-          <div className="account__item">1021-1380059</div>
-          <div className="account__item">IBAN</div>
-          <div className="account__item">TR12000640000011380059</div>
-        </section>
-
-        <section className="account">
-          <h5 className="account__item">TL HESABI</h5>
-          <div className="account__item">1021-1380059</div>
-          <div className="account__item">IBAN</div>
-          <div className="account__item">TR12000640000011380059</div>
-        </section>
-
-        <section className="account">
-          <h5 className="account__item">TL HESABI</h5>
-          <div className="account__item">1021-1380059</div>
-          <div className="account__item">IBAN</div>
-          <div className="account__item">TR12000640000011380059</div>
-        </section>
-      </div>
-
-      <div className="info">
-        
+      </main>
+      <div className="info mt-4">
+        <h3>Barınma desteği sağlayacak bir konteynerin bedeli 45.000 TL</h3>
       </div>
     </div>
   );
